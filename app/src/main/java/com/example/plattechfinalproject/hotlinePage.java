9d220517987a24324088d409434d1c149fd5f247
TIPRaymondOren <mrjoren@tip.edu.ph>
2023-05-03 23:49:56 +0800

    - Modified hotlinePage.java to allow the button to go next - Added java and xml file for journal entry - Modified background of activity_journal_entry.xml

--- app/src/main/java/com/example/plattechfinalproject/hotlinePage.java
+++ app/src/main/java/com/example/plattechfinalproject/hotlinePage.java
@@ -2,12 +2,26 @@ package com.example.plattechfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class hotlinePage extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hotlines_page);
+
+        final Button button = (Button) findViewById(R.id.nextBtnHotline);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actChange = new Intent(hotlinePage.this, end.class);
+
+                startActivity(actChange);
+            }
+        });
     }
 }
