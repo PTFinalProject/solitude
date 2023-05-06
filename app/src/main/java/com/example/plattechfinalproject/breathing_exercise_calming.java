b73881db57b6a0702b8cf3c749f1714a742d203e
TIPRaymondOren <mrjoren@tip.edu.ph>
2023-05-06 15:23:10 +0800

    - Edited title text in activity_journal_entry.xml - Changed button color of "Skip" button in activity_journal_entry.xml - Edited strings.xml

---
+++ app/src/main/java/com/example/plattechfinalproject/breathing_exercise_calming.java
@@ -0,0 +1,14 @@
+package com.example.plattechfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class breathing_exercise_calming extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breathing_exercise_calming);
+    }
+}
