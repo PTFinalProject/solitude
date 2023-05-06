df7c41e3c1178bc6dc861a7b6bafc88b006da4f8
Nichole Sales <mcnsales@tip.edu.ph>
2023-05-07 01:22:47 +0800

    added functionality to buttons memes and end button

--- app/src/main/java/com/example/plattechfinalproject/memes.java
+++ app/src/main/java/com/example/plattechfinalproject/memes.java
@@ -2,7 +2,10 @@ package com.example.plattechfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class memes extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class memes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memes);
+
+        final Button button = (Button) findViewById(R.id.btnMeme);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actChange = new Intent(memes.this, ReadyPage.class);
+                startActivity(actChange);
+            }
+        });
     }
 }
