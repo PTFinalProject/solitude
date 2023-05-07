70159a75a0c252b95e15c57759cbe21458c9c04d
Anshun Yu <anshunyu11@gmail.com>
2023-05-07 12:57:02 +0800

    Started CheckingInPage -Implemented the Layout -Errors and warnings to be fixed

---
+++ app/src/main/java/com/example/plattechfinalproject/CheckingInPage.java
@@ -0,0 +1,14 @@
+package com.example.plattechfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckingInPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checking_in_page);
+    }
+}
