a70dbb5672147c3b45b7739e3d5a5ddd2f921812
Nichole Sales <mcnsales@tip.edu.ph>
2023-05-07 22:14:55 +0800

    finished layout and implemented countdown timer to stretching1 page

--- app/src/main/java/com/example/plattechfinalproject/LandingPage.java
+++ app/src/main/java/com/example/plattechfinalproject/LandingPage.java
@@ -17,7 +17,7 @@ public class LandingPage extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LandingPage.this, MotivationalPage.class);
+                Intent intent = new Intent(LandingPage.this, stretching1.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/plattechfinalproject/stretching1.java
+++ app/src/main/java/com/example/plattechfinalproject/stretching1.java
@@ -3,6 +3,11 @@ package com.example.plattechfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class stretching1 extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class stretching1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stretching1);
+
+        // Initializing thetextView
+        TextView textView = findViewById (R.id.timerTextView);
+
+        new CountDownTimer(31000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // Used for formatting digit to be in 2 digits only
+                NumberFormat f = new DecimalFormat("00");
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+                textView.setText(f.format(min) + ":" + f.format(sec));
+            }
+            // When the task is over it will print 00:00:00 there
+            public void onFinish() {
+                textView.setText("00:00");
+            }
+        }.start();
+
     }
+
+
 }
